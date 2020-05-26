0995c73e74aabd88009f4ced7e77ea1e7ea41e45
OmarGuajardo <omar.guajardog@outlook.com>
2020-05-26 17:09:05 -0500

    change of layout

---
+++ src/Generator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Generator({i}) {
+
+    
+    return (
+        <div>
+            {i}
+        </div>
+    )
+}
--- src/RowInput.js
+++ src/RowInput.js
@@ -3,30 +3,28 @@ import React from 'react'
 
 export default function RowInput(props) {
 
-    const{
-        description,
-        list_items,
-        onChangeItem,
-    } = props
+    const {description, list_items, onChangeItem} = props
 
 
-    if(list_items.length !== 0)
-    {
+    if (list_items.length !== 0) {
         return (
-            <div className = "RowInput">
-                <div>{description}:</div>
-                <select onChange = {onChangeItem}>
-                    {list_items.map(item =>{
-                        return(
-                        <option key = {item} value = {item}>{item}</option>
-                        )
-                    })}
-                </select>
-            </div>
+            <div className="RowInput">
+                    <select onChange={onChangeItem}>
+                        {
+                        list_items.map(item => {
+                            return (
+                                <option key={item}
+                                    value={item}>
+                                    {item}</option>
+                            )
+                        })
+                    } </select>
+                    </div>
+
         )
-    }
-    else
-    {
-        return(<div></div>)
+    } else {
+        return (
+            <div></div>
+        )
     }
 }
