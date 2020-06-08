5f2776dc4500ad1ef07bc8300772cd7b0860dd03
OmarGuajardo <omar.guajardog@outlook.com>
2020-06-08 11:12:49 -0500

    added a 'backend'

--- src/FitGenerator.js
+++ src/FitGenerator.js
@@ -3,34 +3,136 @@ import React, { Component } from 'react'
 import Closet from './Closet'
 import ChoiceSelectorContainer from './components/FitGenerator/ChoiceSelectorContainer'
 import ClassSelector from './components/FitGenerator/ClassSelector'
+import FitMatching from './backend/FitMatching'
 
+// let closet = {Top:["asdf"],Bottom:["105"],Shoes:[234]}
+// let newFitMatcher = new FitMatching(closet)
 
 export default class ToDoApp extends Component {
+
     constructor(props){
         super(props);
         this.handleOnChange = this.handleOnChange.bind(this)
         this.handleChangeClass = this.handleChangeClass.bind(this)
+        this.handleGenerate = this.handleGenerate.bind(this)
+
         this.state = {
             class : "Top",
-            closet : {Top:[],Bottom:[],Shoes:[]},
+            generated_outfits :[],
+            // closet : {Top:[],Bottom:[],Shoes:[]},
+            closet : {
+                Top:[
+                    {
+                        Name: "Horizontal Red Stripes",
+                        Color:"Red",
+                        Fit:"Tight",
+                        Style:"Pattern"
+                    },
+                    {
+                        Name: "Culutre",
+                        Color:"Blue",
+                        Fit:"Big",
+                        Style:"Graphic"
+                    },
+                    {
+                        Name: "Fila",
+                        Color:"White",
+                        Fit:"Big",
+                        Style:"Graphic"
+                    },
+                    {
+                        Name: "Pink Basic Shirt",
+                        Color:"Pink",
+                        Fit:"Fitted",
+                        Style:"Basic"
+                    },
+                    {
+                        Name: "Bright Yellow Striped",
+                        Color:"Yello",
+                        Fit:"Fitted",
+                        Style:"Pattern"
+                    },
+                    {
+                        Name: "Basic Tan",
+                        Color:"Tan",
+                        Fit:"Fitted",
+                        Style:"Basic"
+                    },
+                    {
+                        Name: "Chevron Stripes",
+                        Color:"Blue",
+                        Fit:"Tight",
+                        Style:"Pattern"
+                    }
+                ],
+                Bottom:[
+                    {
+                        Name:"Blue Shorts",
+                        Color:"Blue",
+                        Fit:"Straight",
+                        Style:"Shorts"
+                    },
+                    {
+                        Name:"Ripped Black",
+                        Color:"Black",
+                        Fit:"Ripped",
+                        Style:"Denim"
+                    },
+                    {
+                        Name:"Skinny Dark Blue",
+                        Color:"Blue",
+                        Fit:"Skinny",
+                        Style:"Denim"
+                    },
+                    {
+                        Name:"Khaki Chinos",
+                        Color:"Tan",
+                        Fit:"Skinny",
+                        Style:"Chinos"
+                    },
+                    {
+                        Name:"Khaki Shorts",
+                        Color:"Tan",
+                        Fit:"Straight",
+                        Style:"Shorts"
+                    },
+                ],
+                Shoes:[
+                    {
+                        Name:"Old Skool Vans",
+                        Color:"Black",
+                        ShoeBrand:"Vans"
+                    },
+                    {
+                        Name:"High Tops",
+                        Color:"White",
+                        ShoeBrand:"Rebook"
+                    },
+                    {
+                        Name:"Continental",
+                        Color:"Tan",
+                        ShoeBrand:"Adidas"
+                    },
+                ]
+            },
             item :{},
         }
         this.atts = {
             Top : [
                 {Name :[]},
-                {Color :["Red","Green","Blue","Tan"]},
-                {Fit : ["Fitted","Big"]},
-                {Style :["Graphic","Basic","Button","Long Sleeve"]}
+                {Color :["Red","Green","Blue","Yellow","Pink","Purple","White","Black","Brown","Oragne","Grey","Tan"]},
+                {Fit : ["Tight","Fitted","Oversize"]},
+                {Style :["Graphic","Pattern","Basic","Button","Long Sleeve"]}
             ],
             Bottom: [
                 {Name :[]},
-                {Color :["Red","Green","Blue","Tan"]},
+                {Color :["Red","Green","Blue","Yellow","Pink","Purple","White","Black","Brown","Oragne","Grey","Tan"]},
                 {Fit: ["Straight","Skinny","Ripped"]},
-                {Style: ["Khaki","Slacks","Denim","Joggers","Shorts"]}
+                {Style: ["Khaki","Slacks","Denim","Joggers","Shorts","Chinos"]}
             ],
             Shoes: [
                 {Name :[]},
-                {Color :["Red","Green","Blue","Tan"]},
+                {Color :["Red","Green","Blue","Yellow","Pink","Purple","White","Black","Brown","Oragne","Grey","Tan"]},
                 {ShoeBrand:[]}
             ]
         }
@@ -38,6 +140,15 @@ export default class ToDoApp extends Component {
 
     }
 
+    handleGenerate(){
+        let newCloset = new FitMatching(this.state.closet)
+        let newGeneratedOutfits = newCloset.generateCloset(20)
+        this.setState(prevState =>{
+            return({...prevState,generated_outfits:newGeneratedOutfits})
+        })
+        console.log(this.state.generated_outfits)
+
+    }
     handleAddItem()
     {
         const item = this.state.item
@@ -111,7 +222,8 @@ export default class ToDoApp extends Component {
                         />
                         <button  className = "ChoiceSelectorRow" onClick = {()=>{this.handleAddItem()}}>Add Item</button>
                     </div>
-                    <button onClick = {()=>console.log(this.state.closet)}>Show Closet</button>
+                    <button onClick = {()=>console.log(this.state)}>Show Closet</button>
+                    <button onClick = {()=>this.handleGenerate()}>Generate Outifts</button>
                     <Closet
                     closet = {this.state.closet}
                     />
---
+++ src/backend/FitMatching.js
@@ -0,0 +1,50 @@
+export default class FitMatching{
+    constructor(closet){
+        this.top = closet.Top
+        this.bottom = closet.Bottom
+        this.shoes = closet.Shoes
+    }   
+
+    _getRandomItem(array){
+
+    }
+    generateCloset(number_of_outfits){
+        let local_top = [...this.top]
+        let local_bottom = [...this.bottom]
+        let local_shoe = [...this.shoes]
+        let generated_outfits = []
+        for (let i = 0; i < number_of_outfits; i++) {
+
+            //refilling array
+            if(local_top.length < 1){
+                local_top = [...this.top]
+            }
+            if(local_bottom.length < 1){
+                local_bottom = [...this.bottom]
+            }
+            if(local_shoe.length < 1)
+            {
+                local_shoe = [...this.shoes]
+            }
+
+            //choosing random tops,bottoms and shoes
+            let random_top = local_top[Math.floor(Math.random()*local_top.length)]
+            let random_bottom = local_bottom[Math.floor(Math.random()*local_bottom.length)]
+            let random_shoe = local_shoe[Math.floor(Math.random()*local_shoe.length)]
+
+            let newFit = {
+                Top:random_top,
+                Bottom:random_bottom,
+                Shoe:random_shoe
+            }
+            generated_outfits.push(newFit)
+            //removing the items
+            local_top.splice(local_top.indexOf(random_top),1)
+            local_bottom.splice(local_bottom.indexOf(random_bottom),1)
+            local_shoe.splice(local_shoe.indexOf(random_shoe),1)
+
+        }
+        console.log(generated_outfits)
+        return(generated_outfits)
+    }
+}
