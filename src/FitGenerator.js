6a43d270e2eefa7f7cfb616a16785c608bc8c637
OmarGuajardo <omar.guajardog@outlook.com>
2020-06-11 11:37:19 -0500

    added a local storage system to the close, note: ability to delte hasn't been incorporated

--- src/FitGenerator.js
+++ src/FitGenerator.js
@@ -6,10 +6,7 @@ import ClassSelector from './components/FitGenerator/ClassSelector'
 import FitMatching from './backend/FitMatching'
 import OutfitHandle from './components/GeneratedOutfits/OutfitHandle'
 import GenerateUI from './components/FitGenerator/GenerateUI'
-
-
-// let closet = {Top:["asdf"],Bottom:["105"],Shoes:[234]}
-// let newFitMatcher = new FitMatching(closet)
+import uuid from 'react-uuid'
 
 export default class ToDoApp extends Component {
 
@@ -18,106 +15,109 @@ export default class ToDoApp extends Component {
         this.handleOnChange = this.handleOnChange.bind(this)
         this.handleChangeClass = this.handleChangeClass.bind(this)
         this.handleGenerate = this.handleGenerate.bind(this)
+        this.handleDeleteItem = this.handleDeleteItem.bind(this)
+        this.STORAGE_KEY = "fitGenerator.closet"
 
         this.state = {
+            testCloset :{},
             class : "Top",
             generated_outfits :[],
-            // closet : {Top:[],Bottom:[],Shoes:[]},
-            closet : {
-                Top:[
-                    {
-                        Name: "Horizontal Red Stripes",
-                        Color:"Red",
-                        Fit:"Tight",
-                        Style:"Pattern"
-                    },
-                    {
-                        Name: "Culutre",
-                        Color:"Blue",
-                        Fit:"Big",
-                        Style:"Graphic"
-                    },
-                    {
-                        Name: "Fila",
-                        Color:"White",
-                        Fit:"Big",
-                        Style:"Graphic"
-                    },
-                    {
-                        Name: "Pink Basic Shirt",
-                        Color:"Pink",
-                        Fit:"Fitted",
-                        Style:"Basic"
-                    },
-                    {
-                        Name: "Bright Yellow Striped",
-                        Color:"Yello",
-                        Fit:"Fitted",
-                        Style:"Pattern"
-                    },
-                    {
-                        Name: "Basic Tan",
-                        Color:"Tan",
-                        Fit:"Fitted",
-                        Style:"Basic"
-                    },
-                    {
-                        Name: "Chevron Stripes",
-                        Color:"Blue",
-                        Fit:"Tight",
-                        Style:"Pattern"
-                    }
-                ],
-                Bottom:[
-                    {
-                        Name:"Blue Shorts",
-                        Color:"Blue",
-                        Fit:"Straight",
-                        Style:"Shorts"
-                    },
-                    {
-                        Name:"Ripped Black",
-                        Color:"Black",
-                        Fit:"Ripped",
-                        Style:"Denim"
-                    },
-                    {
-                        Name:"Skinny Dark Blue",
-                        Color:"Blue",
-                        Fit:"Skinny",
-                        Style:"Denim"
-                    },
-                    {
-                        Name:"Khaki Chinos",
-                        Color:"Tan",
-                        Fit:"Skinny",
-                        Style:"Chinos"
-                    },
-                    {
-                        Name:"Khaki Shorts",
-                        Color:"Tan",
-                        Fit:"Straight",
-                        Style:"Shorts"
-                    },
-                ],
-                Shoes:[
-                    {
-                        Name:"Old Skool Vans",
-                        Color:"Black",
-                        ShoeBrand:"Vans"
-                    },
-                    {
-                        Name:"High Tops",
-                        Color:"White",
-                        ShoeBrand:"Rebook"
-                    },
-                    {
-                        Name:"Continental",
-                        Color:"Tan",
-                        ShoeBrand:"Adidas"
-                    },
-                ]
-            },
+            closet : {Top:[],Bottom:[],Shoes:[]},
+            // closet : {
+            //     Top:[
+            //         {
+            //             Name: "Horizontal Red Stripes",
+            //             Color:"Red",
+            //             Fit:"Tight",
+            //             Style:"Pattern"
+            //         },
+            //         {
+            //             Name: "Culutre",
+            //             Color:"Blue",
+            //             Fit:"Big",
+            //             Style:"Graphic"
+            //         },
+            //         {
+            //             Name: "Fila",
+            //             Color:"White",
+            //             Fit:"Big",
+            //             Style:"Graphic"
+            //         },
+            //         {
+            //             Name: "Pink Basic Shirt",
+            //             Color:"Pink",
+            //             Fit:"Fitted",
+            //             Style:"Basic"
+            //         },
+            //         {
+            //             Name: "Bright Yellow Striped",
+            //             Color:"Yello",
+            //             Fit:"Fitted",
+            //             Style:"Pattern"
+            //         },
+            //         {
+            //             Name: "Basic Tan",
+            //             Color:"Tan",
+            //             Fit:"Fitted",
+            //             Style:"Basic"
+            //         },
+            //         {
+            //             Name: "Chevron Stripes",
+            //             Color:"Blue",
+            //             Fit:"Tight",
+            //             Style:"Pattern"
+            //         }
+            //     ],
+            //     Bottom:[
+            //         {
+            //             Name:"Blue Shorts",
+            //             Color:"Blue",
+            //             Fit:"Straight",
+            //             Style:"Shorts"
+            //         },
+            //         {
+            //             Name:"Ripped Black",
+            //             Color:"Black",
+            //             Fit:"Ripped",
+            //             Style:"Denim"
+            //         },
+            //         {
+            //             Name:"Skinny Dark Blue",
+            //             Color:"Blue",
+            //             Fit:"Skinny",
+            //             Style:"Denim"
+            //         },
+            //         {
+            //             Name:"Khaki Chinos",
+            //             Color:"Tan",
+            //             Fit:"Skinny",
+            //             Style:"Chinos"
+            //         },
+            //         {
+            //             Name:"Khaki Shorts",
+            //             Color:"Tan",
+            //             Fit:"Straight",
+            //             Style:"Shorts"
+            //         },
+            //     ],
+            //     Shoes:[
+            //         {
+            //             Name:"Old Skool Vans",
+            //             Color:"Black",
+            //             ShoeBrand:"Vans"
+            //         },
+            //         {
+            //             Name:"High Tops",
+            //             Color:"White",
+            //             ShoeBrand:"Rebook"
+            //         },
+            //         {
+            //             Name:"Continental",
+            //             Color:"Tan",
+            //             ShoeBrand:"Adidas"
+            //         },
+            //     ]
+            // },
             item :{},
         }
         this.atts = {
@@ -139,8 +139,14 @@ export default class ToDoApp extends Component {
                 {ShoeBrand:[]}
             ]
         }
-        this.STORAGE_KEY = "KEY"
 
+
+
+    }
+
+    handleDeleteItem(id){
+        //TODO: Delete Item from this.state.closet based on ID
+        //componentDidUpdate should take care of saving it to the localStorage
     }
 
     handleGenerate(numFits){
@@ -161,11 +167,39 @@ export default class ToDoApp extends Component {
         }
         else
         {
+            // console.log("handling adding item")
+            // const newItem = {...this.state.item,ID:uuid()}
+            // console.log(newItem)
+
             this.setState(prevState =>{
-                const newAddition = [...prevState.closet[this.state.class],this.state.item]
+                const newItem = {...this.state.item,ID:uuid()}
+                const newAddition = [...prevState.closet[this.state.class],newItem]
                 const newCloset = {...prevState.closet,[this.state.class]:newAddition}
                 return({...prevState,closet:newCloset})
             })
+        }
+    }
+    componentDidUpdate(prevProps,prevState,snapshot)
+    {
+        if(this.state.closet !== prevState.closet){
+            console.log("saving new closet")
+            localStorage.setItem(this.STORAGE_KEY,JSON.stringify(this.state.closet))
+        }
+        else{
+            console.log("closet has not changed")
+        }
+    }
+    componentDidMount()
+    {
+        const retrievedCloset = JSON.parse(localStorage.getItem(this.STORAGE_KEY))
+        if(retrievedCloset){
+            this.setState(prevState =>{
+                return({...prevState,closet:retrievedCloset})
+            })
+        }
+        else
+        {
+            console.log("nothing retrieved")
         }
     }
     handleChangeClass(e)
@@ -234,9 +268,6 @@ export default class ToDoApp extends Component {
                         />
                         <button  className = "ChoiceSelectorRow" onClick = {()=>{this.handleAddItem()}}>Add Item</button>
                     </div>
-                    {/* <button onClick = {()=>console.log(this.state)}>Show Closet</button>
-                    <button onClick = {()=>this.handleGenerate()}>Generate Outifts</button>
-                    <button onClick = {()=>console.log(this.state.generated_outfits)}>Show Outifts</button> */}
                     <Closet
                     closet = {this.state.closet}
                     />
