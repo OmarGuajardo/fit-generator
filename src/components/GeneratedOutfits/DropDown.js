33a4ae688e7b920f3901cca41e1272e06968a29a
OmarGuajardo <omar.guajardog@outlook.com>
2020-06-12 09:55:02 -0500

    small chnaged to the drop down component so that it doesn't show the ID

--- src/components/GeneratedOutfits/DropDown.js
+++ src/components/GeneratedOutfits/DropDown.js
@@ -7,7 +7,13 @@ export default function DropDown(props) {
             <div className="dropdown-btn">{props.title}</div>
             <div  className="dropdown-content">
             {Object.keys(props.info).map(key =>{
-              return(<div key = {props.info[key]}><strong>{key}: </strong>  {props.info[key]}</div>)
+                if(key !== "ID"){
+                    return(<div key = {props.info[key]}><strong>{key}: </strong>  {props.info[key]}</div>)
+                }
+                else
+                {
+                    return(null)
+                }
                 })}
             </div>
         </div>
